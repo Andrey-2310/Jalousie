1375c138e79a0c9a6226f1d8d3d15cd0cbbf4bc1
Andrey Redkovskiy <andreyredkovskiy@mail.ru>
2018-07-16 22:13:21 +0300

    servo with bluetooth

--- jalouzie.ino
+++ jalouzie.ino
@@ -1,20 +1,51 @@
-int clockwisePin = 6;
-int counterClockwisePin = 7;
+#include <Servo.h>
+
+Servo myservo;
+
+int servoPin = 9;
+
+int servoState = 90;
+
+char data;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(clockwisePin, OUTPUT);
-  pinMode(counterClockwisePin, OUTPUT);
+  myservo.attach(servoPin);
+  Serial.begin(9600);
 }
 
 void loop() {
-  iteration(clockwisePin, counterClockwisePin);
-  iteration(counterClockwisePin, clockwisePin);
+  checkReceivedData();
+  myservo.write(servoState);
 }
 
-void iteration(int kek, int lol) {
-  digitalWrite(lol, HIGH);
-  digitalWrite(kek, LOW);
-  delay(3000);
+void checkReceivedData() {
+  if (Serial.available() > 0)
+  {
+    data = Serial.read();     
+    Serial.print(data);          
+    Serial.print("\n");
+    if (data == '1') {
+      servoLeftRotate();
+    }
+    else if (data == '2')  {
+      servoRightRotate();
+    }
+     else if (data == '0')  {
+      servoStop();
+    }
+  }
 }
+
+void servoLeftRotate() {
+  servoState = 180;
+}
+
+void servoRightRotate() {
+  servoState = 0;
+}
+
+void servoStop() {
+  servoState = 90;
+}
+
 
